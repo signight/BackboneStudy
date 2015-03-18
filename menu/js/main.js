a8f93470f5950028a537a08f1262b5f3326000ef
nightsign <zhangsunan1982@gmail.com>
2015-03-18 17:59:32 +0800

    add menu

---
+++ menu/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+require.config({
+	paths:{
+		jquery: 'lib/jquery.min',
+		underscore: 'lib/underscore',
+		backbone: 'lib/backbone-min',
+		backboneLocalstorage: 'lib/backbone.localStorage-min',
+		text: 'lib/text'
+	},
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: [
+				'underscore',
+				'jquery'
+			],
+			exports: 'Backbone'
+		},
+		backboneLocalstorage: {
+			deps: ['backbone'],
+			exports: 'Store'
+		}
+	}
+});
+require(['backbone','views/app'],function  (Backbone,Appview) {
+	new Appview();
+})
